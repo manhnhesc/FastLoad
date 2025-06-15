9508fe5d1eddee15c7829d3c4e9e57f4198da627
manhnhesc <manhnhesc@yahoo.com>
2025-06-15 15:59:01 +0700

    u

--- inject.js
+++ inject.js
@@ -1,4 +1,5 @@
 const editorExtensionId = 'pffkfjfjdklhlkdpjmjhgpggaecjemdm';
+var port = chrome.runtime.connect(editorExtensionId, { name: "TikTokConnect" });
 const windowFetch = window.fetch;
 window.fetch = async (...i) => {
     var [t] = i;
@@ -6,27 +7,27 @@ window.fetch = async (...i) => {
     const windowFetchPromise = windowFetch(t);
     const windowFetchResult = await windowFetchPromise, cloneFetch = windowFetchResult.clone();
     try {
-        if (windowFetchResult.ok && ((contentType = windowFetchResult.headers.get("Content-Type")) != null && contentType.includes("application/json"))) {
-            if (windowFetchResult.url.indexOf('https://www.tiktok.com/api/post/item_list') !== -1) {
-                const cloneFetchJson = await cloneFetch.json();
-                try {
-                    var port = await chrome.runtime.connect(editorExtensionId, { name: "TikTokConnect" });
-                    await port.postMessage({ localstorage: "TikTokDownload" });
-                    await port.onMessage.addListener(tiktokDownloadResponse => {
-                        if (tiktokDownloadResponse.TikTokDownload) {
+        await port.postMessage({ localstorage: "TikTokDownload" });
+        await port.onMessage.addListener(async tiktokDownloadResponse => {
+            if (tiktokDownloadResponse.TikTokDownload) {
+                if (windowFetchResult.ok && ((contentType = windowFetchResult.headers.get("Content-Type")) != null && contentType.includes("application/json"))) {
+                    if (windowFetchResult.url.indexOf('https://www.tiktok.com/api/post/item_list') !== -1) {
+                        try {
+                            const cloneFetchJson = await cloneFetch.json();
+
                             console.log("Data event sending")
                             const dispatchEvent = new CustomEvent('DataEvent', { detail: cloneFetchJson });
                             window.dispatchEvent(dispatchEvent)
+                        } catch (e) {
+                            console.log(e)
                         }
-                    });
-                } catch (e) {
-                    console.log(e)
-                }
-            } else {
-                //console.log(windowFetchResult.url)
+                    } else {
+                        console.log(windowFetchResult.url)
+                    }
+                } else
+                    console.error("Response is not JSON or not ok:", windowFetchResult)
             }
-        } else
-            console.error("Response is not JSON or not ok:", windowFetchResult)
+        });
     } catch (s) {
         console.log(s)
     }
